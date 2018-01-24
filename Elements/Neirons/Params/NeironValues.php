cf8c6ae717a4077d3e39cca3915c090b0c3e4157
prorok12342 <Sadam-333@yandex.ru>
2018-01-24 12:04:03 +0300

    Добавлены инициализирующие параметры

---
+++ Elements/Neirons/Params/NeironValues.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Класс, определяющий веса нейронов.
+ */
+
+namespace neirons;
+
+/**
+ * Description of NeironValues
+ *
+ * @author ANDREY BUIMOV
+ */
+class NeironValues
+{
+    public function __construct($param)
+    {
+        
+    }
+}
